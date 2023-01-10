5e8385b559ae2209c6f3beaa0db9ff16c0b31172
Mohini Mahato <mohinimahatowork@gmail.com>
2023-01-10 23:59:37 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from "react-dom/client";
+import SearchParams from "./components/images/SearchParams";
 
 const App = () => {
-  return <div>PUP Care</div>;
+  return (
+    <div>
+      <div>PUP Care</div>
+      <SearchParams />
+    </div>
+  );
 };
 const container = document.getElementById("root");
 const root = createRoot(container);
---
+++ src/components/images/SearchParams.jsx
@@ -0,0 +1,13 @@
+const SearchParams = () => {
+  return (
+    <div>
+      <div className="col-8 mx-auto">
+        <form>
+          <input className="input" />
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default SearchParams();
