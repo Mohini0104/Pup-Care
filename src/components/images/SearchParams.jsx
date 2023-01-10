3a5b619e14f7f0f6803dd280ffec6e78a20586f1
Mohini Mahato <mohinimahatowork@gmail.com>
2023-01-11 00:19:51 +0530

    import useState for setting and updating location

--- src/components/images/SearchParams.jsx
+++ src/components/images/SearchParams.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 const SearchParams = () => {
+  const [location, setLocation] = useState("");
   return (
     <div>
       <div className="col-8 mx-auto">
         <form>
-          <input className="input" />
+          <label htmlFor="location">Location</label>
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            className="location-input"
+            id="location"
+            placeholder="Location"
+            value={location}
+          />
           <button>Submit</button>
         </form>
       </div>
     </div>
   );
 };
-export default SearchParams();
+export default SearchParams;
