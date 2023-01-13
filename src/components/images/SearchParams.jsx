8cbd8709f0870034d100e2738d13faa51774d36a
Mohini Mahato <mohinimahatowork@gmail.com>
2023-01-13 20:44:35 +0530

    Started using useState hook

--- src/components/images/SearchParams.jsx
+++ src/components/images/SearchParams.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  /*
+  const locationHook = useState("");
+  const location = locationHook[0];
+  const setLocation = locationHook[1];
+  */
   return (
     <div>
       <div className="col-8 mx-auto">
@@ -13,6 +20,22 @@ const SearchParams = () => {
             placeholder="Location"
             value={location}
           />
+          <label htmlFor="animal">
+            Animal
+            <select
+              name="animal"
+              id="animal"
+              value={animal}
+              onChange={(e) => {
+                setAnimal(e.target.value);
+              }}
+            >
+              {/* <option /> */}
+              {ANIMALS.map((animal) => (
+                <option key={animal}>{animal}</option>
+              ))}
+            </select>
+          </label>
           <button>Submit</button>
         </form>
       </div>
